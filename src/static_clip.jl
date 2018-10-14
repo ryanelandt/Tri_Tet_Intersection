63571152cf7aac7b4aadfa4e6954ef52e64fdeb5
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-14 01:05:18 -0400

    updated tests

--- src/static_clip.jl
+++ src/static_clip.jl
@@ -1,52 +1,52 @@
 
 # clipping functions assume that the triangle does not lie on a side of the tet
 
-# clip_in_tet_coordinates(z::NTuple{N,SVector{4,T}}) where {N,T} = clip_by_tet_plane(z, 1)
-#
-# function clip_by_tet_plane(z1::SVector{4,T}, z2::NTuple{N,SVector{4,T}}, z3::SVector{4,T}, i::Int64) where {N,T}
-#     z = (z1, z2..., z3)
-#     return clip_by_tet_plane(z, i)
-# end
-#
-# function tuple_forward(z::NTuple{N,SVector{4,T}}, i::Int64) where {N, T}
-#     return Tuple(z[mod1(k + i - 1, N)] for k = 1:N)
-# end
-#
-# function clip_by_tet_plane(z::NTuple{N,SVector{4,T}}, i::Int64) where {N,T}
-#     (i == 5) && (return true, z)
-#     s = Tuple(z[k][i] for k = 1:N)
-#     is_non_pos = (s .<= 0.0)
-#     all(is_non_pos) && (return false, NTuple{0,SVector{4,T}}())
-#     if all(0.0 .<= s)  # non_negative
-#         return clip_by_tet_plane(z, i + 1)
-#     else
-#         for k = 1:N
-#             if is_non_pos[k] && !is_non_pos[mod1(k + 1, N)]
-#                 z = tuple_forward(z, k)
-#                 return cut_in_tet_coordinates(z, i)
-#             end
-#         end
-#     end
-#     error("this code should be unreachable")
-# end
-#
-# function cut_in_tet_coordinates(z::NTuple{N,SVector{4,T}}, i::Int64) where {N,T}
-#     (z[N-1][i] <= 0.0) && (return clip_by_tet_plane(z[1:(N - 1)], i))
-#     z_start = clip_node(z[1], z[2], i)
-#     if 0.0 < z[N][i]
-#         z_end = clip_node(z[1], z[N], i)
-#         return clip_by_tet_plane(z_start, z[2:N], z_end, i + 1)
-#     else
-#         z_end = clip_node(z[N], z[N-1], i)
-#         return clip_by_tet_plane(z_start, z[2:(N-1)], z_end, i + 1)
-#     end
-# end
-#
-# function clip_node(z_non::SVector{4,T}, z_pos::SVector{4,T}, i::Int64) where {T}
-#     v_non = z_non[i]
-#     v_pos = z_pos[i]
-#     return weightPoly(z_non, z_pos, v_non, v_pos)
-# end
+clip_in_tet_coordinates(z::NTuple{N,SVector{4,T}}) where {N,T} = clip_by_tet_plane(z, 1)
+
+function clip_by_tet_plane(z1::SVector{4,T}, z2::NTuple{N,SVector{4,T}}, z3::SVector{4,T}, i::Int64) where {N,T}
+    z = (z1, z2..., z3)
+    return clip_by_tet_plane(z, i)
+end
+
+function tuple_forward(z::NTuple{N,SVector{4,T}}, i::Int64) where {N, T}
+    return Tuple(z[mod1(k + i - 1, N)] for k = 1:N)
+end
+
+function clip_by_tet_plane(z::NTuple{N,SVector{4,T}}, i::Int64) where {N,T}
+    (i == 5) && (return true, z)
+    s = Tuple(z[k][i] for k = 1:N)
+    is_non_pos = (s .<= 0.0)
+    all(is_non_pos) && (return false, NTuple{0,SVector{4,T}}())
+    if all(0.0 .<= s)  # non_negative
+        return clip_by_tet_plane(z, i + 1)
+    else
+        for k = 1:N
+            if is_non_pos[k] && !is_non_pos[mod1(k + 1, N)]
+                z = tuple_forward(z, k)
+                return cut_in_tet_coordinates(z, i)
+            end
+        end
+    end
+    error("this code should be unreachable")
+end
+
+function cut_in_tet_coordinates(z::NTuple{N,SVector{4,T}}, i::Int64) where {N,T}
+    (z[N-1][i] <= 0.0) && (return clip_by_tet_plane(z[1:(N - 1)], i))
+    z_start = clip_node(z[1], z[2], i)
+    if 0.0 < z[N][i]
+        z_end = clip_node(z[1], z[N], i)
+        return clip_by_tet_plane(z_start, z[2:N], z_end, i + 1)
+    else
+        z_end = clip_node(z[N], z[N-1], i)
+        return clip_by_tet_plane(z_start, z[2:(N-1)], z_end, i + 1)
+    end
+end
+
+function clip_node(z_non::SVector{4,T}, z_pos::SVector{4,T}, i::Int64) where {T}
+    v_non = z_non[i]
+    v_pos = z_pos[i]
+    return weightPoly(z_non, z_pos, v_non, v_pos)
+end
 
 
 
--- test/test_exports.jl
+++ test/test_exports.jl
@@ -2,5 +2,6 @@
     # Ensure that every exported name is actually defined
     for name in names(Tri_Tet_Intersections)
         @test isdefined(Tri_Tet_Intersections, name)
+        !isdefined(Tri_Tet_Intersections, name) && println("MISSING: ", name)
     end
 end
